e8346f44a9b4fad1585841c372960c8b21b261f7
Rich Rose <askrichardrose@gmail.com>
2020-02-01 12:19:28 +0000

    Config update

--- flutter/direct/docker-run.sh
+++ flutter/direct/docker-run.sh
@@ -1,1 +1,1 @@
-docker run -d --name flutter-studio --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /usr/lib:/usr/lib flutter-studio-bionic:0.1
+docker run -d --name flutter-studio --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /usr/lib:/usr/lib gcr.io/qwiklabs-resources/flutter_direct:0.1
